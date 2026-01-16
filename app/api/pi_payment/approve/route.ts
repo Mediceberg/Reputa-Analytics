efd691442b83e5a1248a046f3654f38d51b72e06
Mediceberg <elmehdi.meliani1@gmail.com>
2026-01-16 20:40:39 +0100

    Update route.ts

--- app/api/pi_payment/approve/route.ts
+++ app/api/pi_payment/approve/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { approvePOST } from 'pi-sdk-nextjs';
+import { approvePOST } from 'pi-sdk-nextjs'; 
 
 export async function POST(req: NextRequest) {
   // TODO: Fill this handler with Pi logic or call out to your SDK
