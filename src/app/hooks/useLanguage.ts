717efe49e9fc48be7c60cee63a46e2c061ba4055
Mediceberg <elmehdi.meliani1@gmail.com>
2026-01-30 22:24:11 +0100

    Update useLanguage.ts

--- src/app/hooks/useLanguage.ts
+++ src/app/hooks/useLanguage.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'; 
 import { Language } from '../protocol/types';
 import { getTranslation } from '../locales/translations';
 
